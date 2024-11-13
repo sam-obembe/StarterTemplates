df51ee0d5f4ede43b4c6e961b4a6b29271e2cb3a
Samuel Obembe <samuel.obembe@outlook.com>
2024-11-13 11:18:16 -0600

    adding json to express starter api

--- expressApi/dist/app.js
+++ expressApi/dist/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+app.use(express.static("public"));
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+app.get('/swagger', (req, res) => {
+    res.redirect("/swagger.html");
 });
 app.get('/ping', (req, res) => {
     var resVal = {
@@ -10,6 +18,18 @@ app.get('/ping', (req, res) => {
     };
     res.json(resVal);
 });
+app.get("/404", function (req, res) {
+    res.json({ message: "not found" });
+});
+app.get("/500", function (req, res) {
+    res.json({ message: "internal error" });
+});
+app.use((req, res, next) => {
+    res.status(200).json({ data: Object.assign({}, req.body) });
+    next();
+});
+app.use((req, res, next) => { res.status(404).redirect("/404"); });
+app.use((req, res, next) => { res.status(500).redirect("/500"); });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
--- expressApi/src/app.ts
+++ expressApi/src/app.ts
@@ -2,9 +2,20 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static("public"))
+
+app.use((req, res, next) => {
+    console.log(req.url)
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/swagger', (req, res) => {
+    res.redirect("/swagger.html")
+})
 
 app.get('/ping', (req, res) => {
     var resVal: Demo = {
@@ -12,6 +23,21 @@ app.get('/ping', (req, res) => {
     }
     res.json(resVal)
 })
+
+app.get("/404", function (req, res) {
+    res.json({ message: "not found" })
+})
+
+app.get("/500", function (req, res) {
+    res.json({ message: "internal error" })
+})
+
+app.use((req, res, next) => {
+    res.status(200).json({ data: { ...req.body } })
+    next()
+})
+app.use((req, res, next) => { res.status(404).redirect("/404") });
+app.use((req, res, next) => { res.status(500).redirect("/500") });
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
