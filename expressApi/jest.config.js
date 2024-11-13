e589e88fb339c5fc95f24c441d8d7b108e93f828
Samuel Obembe <samuel.obembe@outlook.com>
2024-11-13 17:11:18 -0600

    updating configs for jest

--- expressApi/jest.config.js
+++ expressApi/jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: "node",
+  verbose: false,
+  collectCoverage: true,
+  coverageReporters: ["html"],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
 };
